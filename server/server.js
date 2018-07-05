96b6e896bd54e53c704904a68e811ce47070163e
Leon Matota <leonmatota@gmail.com>
2018-07-05 13:46:00 +0200

    added login

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
-var {authenticate} = require('./middleware/auth');
+var { authenticate } = require('./middleware/auth');
 
 var app = express();
 const port = process.env.PORT || 3005;
@@ -111,6 +111,18 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(port, () => {
     console.log(`started on port ${port} on ${config.env}`);
